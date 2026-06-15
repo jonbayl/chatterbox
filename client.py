92bf6f40209a3a10f9ab8b2b62526eb3be8be60a
jonbayl <30201351+jonbayl@users.noreply.github.com>
2026-06-15 20:48:25 +0100

    chore: documented with doc strings and some refactoring

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
+"""Acts as a client for the Chatterbox Chat Server.
+
+This module handles communication with a Chatterbox server,
+allowing users to send and receive messages in real-time. It 
+supports both secure (SSL/TLS) and insecure connections, 
+depending on the user's preference.
+"""
+
 import argparse
-import socket
 import ssl
 import asyncio
 
@@ -8,27 +15,92 @@ from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit.formatted_text import FormattedText
 
 def chatterbox_client_cliparser():
-    parser = argparse.ArgumentParser(description='Chatterbox Client is a simple network chat client)')
-    parser.add_argument('host', type=str, nargs="?", default="localhost", help='Host address to connect to (default: localhost)')
-    parser.add_argument('--port', type=int, default=2428, help='Port number to connect to (default: 2428)')
-    parser.add_argument('--ssl', type=str, help='Enable SSL/TLS for secure communication (provide "trusted CA cert" to enable SSL)')
+    """Handle command line parsing for the client.
+
+    Configure arguments for the host address, port,
+    and SSL configuration required to connect to the
+    chat server.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments.
+        May contain host, port, and ssl certificate path.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='Chatterbox Client is a simple network chat client)'
+        )
+    parser.add_argument('host', type=str, nargs="?", default="localhost", 
+        help='Host address to connect to (default: localhost)'
+    )
+    parser.add_argument('--port', type=int, default=2428, 
+        help='Port number to connect to (default: 2428)'
+    )
+    parser.add_argument('--ssl', type=str, 
+        help='Enable SSL/TLS for secure communication (provide'
+        ' "trusted CA cert" to enable SSL)'
+    )
     args = parser.parse_args()
 
     return args
 
 async def chatterbox_client_insecure(host, port):
+    """Establish an insecure connection to the server.
+
+    Sets up a raw TCP connection to the specified host and
+    port, without encryption. 
+
+    Args:
+        host (str): The server's hostname or IP address.
+        port (int): The server's port number.
+
+    Returns:
+        tuple: A tuple containing the reader and writer streams 
+        for the connection:
+            - reader: asyncio.StreamReader.
+            - writer: asyncio.StreamWriter.
+    """
+
     reader, writer = await asyncio.open_connection(host, port)
     return reader, writer
 
 async def chatterbox_client_secure(host, port, cert):
+    """Establish an secure connection to the server.
+
+    Sets up a TCP connection to the specified host and
+    port, with SSL/TLS encryption.
+
+    Args:
+        host (str): The server's hostname or IP address.
+        port (int): The server's port number.
+        cert (str): Path to the SSL certificate file for
+        certificate verification.
+
+    Returns:
+        tuple: A tuple containing the reader and writer streams 
+        for the connection:
+            - reader: asyncio.StreamReader.
+            - writer: asyncio.StreamWriter.
+    """
+
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     context.load_verify_locations(cert)
 
-    reader, writer = await asyncio.open_connection(host, port, ssl=context)
-    
+    reader, writer = await asyncio.open_connection(host, port, 
+    ssl=context)
+
     return reader, writer
 
-async def chatterbox_receive(reader, writer):
+async def chatterbox_receive(reader, writer):   
+    """ Receive messages from the server.
+
+    Continuously listens for incoming messages from the server
+    and displays them within the terminal. 
+
+    Args:
+        reader (asyncio.StreamReader): The reader for the connection.
+        writer (asyncio.StreamWriter): The writer for the connection.
+    """
+
     loop = asyncio.get_event_loop()
     try:
         while True:
@@ -46,6 +118,15 @@ async def chatterbox_receive(reader, writer):
         await writer.wait_closed()
 
 async def chatterbox_send(reader, writer):
+    """ Send messages to the server.
+
+    Allows the user to input messages which are then sent to the server.
+
+    Args:
+        reader (asyncio.StreamReader): The reader for the connection.
+        writer (asyncio.StreamWriter): The writer for the connection.
+    """
+    
     session = PromptSession()
     try:
         while True:
@@ -64,10 +145,18 @@ async def chatterbox_send(reader, writer):
         await writer.wait_closed()
 
 async def main():
+    """Execute the primary lifecycle of the Chatterbox client."""
+
+    args = chatterbox_client_cliparser()
+
     if args.ssl:
-        reader, writer = await chatterbox_client_secure(args.host, args.port, args.ssl)
+        reader, writer = await chatterbox_client_secure(args.host, 
+            args.port, args.ssl
+        )
     else:
-        reader, writer = await chatterbox_client_insecure(args.host, args.port)
+        reader, writer = await chatterbox_client_insecure(args.host, 
+            args.port
+        )
 
     with patch_stdout():
         await asyncio.gather(
@@ -76,6 +165,4 @@ async def main():
         )
 
 if __name__ == "__main__":
-    args = chatterbox_client_cliparser()
     asyncio.run(main())
-    
--- server.py
+++ server.py
@@ -1,15 +1,47 @@
+""" Chatterbox Chat Server
+
+This module implements a simple chat server that allows
+multiple clients to connect and send messages which are 
+then broadcast to all other users.
+"""
+
 import argparse
 import socket
 import ssl
 
 def chatterbox_cliparser():
-    parser = argparse.ArgumentParser(description='Chatterbox Server is a simple network chat server)')
-    parser.add_argument('host', type=str, nargs="?", default="localhost", help='Host address to listen on (default: localhost)')
-    parser.add_argument('--port', type=int, default=2428, help='Port number to listen on (default: 12345)')
+    """Handle command line parsing for the server.
 
-    enable_ssl = parser.add_argument_group("ssl", "Enable SSL/TLS for secure communication")
-    enable_ssl.add_argument('--cert', type=str, help="Path to the SSL certificate file (optional)")
-    enable_ssl.add_argument('--key', type=str, help="Path to the SSL key file (optional)")
+    Configure arguments for the host address and port 
+    on which the server will listen, and optional SSL 
+    configuration (cert and key) which can be used to 
+    provide secure communication.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments.
+        May contain host, port, and ssl certificate path.
+    """
+
+    parser = argparse.ArgumentParser(description='Chatterbox Server is a'
+        ' simple network chat server)'
+    )
+    parser.add_argument('host', type=str, nargs="?", 
+        default="localhost", help='Host address to listen on'
+        ' (default: localhost)'
+    )
+    parser.add_argument('--port', type=int, default=2428, 
+        help='Port number to listen on (default: 12345)'
+    )
+
+    enable_ssl = parser.add_argument_group("ssl", 
+        "Enable SSL/TLS for secure communication"
+    )
+    enable_ssl.add_argument('--cert', type=str, 
+        help="Path to the SSL certificate file (optional)"
+    )
+    enable_ssl.add_argument('--key', type=str, 
+        help="Path to the SSL key file (optional)"
+    )
 
     args = parser.parse_args()
 
@@ -20,6 +52,19 @@ def chatterbox_cliparser():
     return args
 
 def chatterbox_setup(host, port):
+    """ Setup an insecure TCP socket
+
+    Configures the server to set up a raw, unencrypted TCP socket
+    on the specified host and port.
+
+    Args:
+        host (str): The hostname or IP address to bind the server to.
+        port (int): The port number to listen on.
+
+    Returns:
+        socket.socket: A configured TCP socket ready to accept connections.
+    """
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((host, port))
     sock.listen()
@@ -28,6 +73,21 @@ def chatterbox_setup(host, port):
     return sock 
 
 def chatterbox_setup_ssl(host, port, cert, key):
+    """ Setup an secure TCP socket
+
+    Configures the server to set up an encrypted TCP socket
+    on the specified host and port.
+
+    Args:
+        host (str): The hostname or IP address to bind the server to.
+        port (int): The port number to listen on.
+        cert (str): Path to the SSL certificate file.
+        key (str): Path to the SSL key file.
+
+    Returns:
+        socket.socket: A configured TCP socket ready to accept connections.
+    """
+
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain(certfile=cert, keyfile=key)
 
@@ -41,6 +101,16 @@ def chatterbox_setup_ssl(host, port, cert, key):
     return sock
 
 def chatterbox_serve(sock):
+    """ Start the server to accept incoming connections.
+
+    Continuously listens for incoming client connections and
+    handles them in a loop. For each connection, it sends a welcome
+    message and echoes back any received messages.
+
+    Args:
+        sock (socket.socket): The server socket to accept connections on.
+    """
+
     while True:
         client_socket, client_address = sock.accept()
         print(f"Connection established with {client_address}")
@@ -50,7 +120,9 @@ def chatterbox_serve(sock):
             data = client_socket.recv(1024)
             if not data:
                 break
-            print(f"Message from {client_address[0]}: {data.decode('utf-8')}")
+            print(f"Message from {client_address[0]}: " 
+                f"{data.decode('utf-8')}"
+            )
             client_socket.sendall(data) 
         
         client_socket.close()
@@ -59,7 +131,9 @@ if __name__ == '__main__':
     args = chatterbox_cliparser()
 
     if args.cert:
-        sock = chatterbox_setup_ssl(args.host, args.port, args.cert, args.key)
+        sock = chatterbox_setup_ssl(args.host, args.port, 
+            args.cert, args.key
+        )
     else:
         sock = chatterbox_setup(args.host, args.port)
 
